59003abd343c1a5b748f00eb49afd1bc4de5459b
Loïc <loic.geinoz@gmail.com>
2019-04-30 17:32:49 +0200

    début (presque fin)

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+   const int SIZE = 0;
+   int* sizePtr = &SIZE;
+   unsigned nbElementsTab, nbLances;
+   char refaire;
+   do
+   {
+      printf("nbre d'elements    [0..100] : ");
+      scanf("%d", nbElementsTab);
+      *sizePtr = nbElementsTab;
+      int tab[SIZE];
+      printf("nbre de lances   [0..10000] : ");
+      scanf("%d", nbLances);
+      randomize(tab, sizeof(tab)/sizeof(unsigned));
+      affichagerTab(tab, sizeof(tab)/sizeof(unsigned));
+      printf("voulez-vous recommencer [o:n] :");
+      scanf("%c", refaire);
+   } while (refaire != 'n');
     return 0;
 }
