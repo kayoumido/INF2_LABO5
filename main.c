51811c0f273f6d3f78a59b116393d0f85630363d
Doran <dorankayoumi@gmail.com>
2019-05-07 11:29:52 +0200

    Light refactor and add comments

--- main.c
+++ main.c
@@ -1,15 +1,46 @@
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : 05
+Fichier     : main.cpp
+Auteur(s)   : Loïc Geinoz, Doran Kayoumi, David Oliveira Monteiro
+Date        : 07.05.2019
+But         : Effectue des statistiques sur des valeurs générées aléatoirement.
+Remarque(s) : -
+Compilateur : MinGW-g++ 6.3.0
+-----------------------------------------------------------------------------------
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
 
-#define VIDER_BUFFER    while( getchar() != '\n')
+#define VIDER_BUFFER while( getchar() != '\n')
 
-void stats(unsigned tab[], unsigned valMax, unsigned nbThrows) {
-
-    for (unsigned i = 0; i < valMax; ++i) {
-        *(tab + i) = 0;
+/**
+ * @brief Initialise an array wit a default value
+ *
+ * @param tab           array to initialise
+ * @param size          size of the array
+ * @param defaultValue  default value to set
+ */
+void initArray(unsigned *tab, unsigned size, unsigned defaultValue) {
+    for (unsigned i = 0; i < size; ++i) {
+        *(tab + i) = defaultValue;
     }
+}
+
+/**
+ * @brief   Fill an array with the number of times a number was
+ *          randomly generated
+ *
+ * @param tab       array to fill
+ * @param valMax    max value that can be generated
+ * @param nbThrows  number of "throws" to do
+ */
+void generateRandom(unsigned *tab, unsigned valMax, unsigned nbThrows) {
+
+    initArray(tab, valMax, 0);
 
     srand(time(0));
 
@@ -21,20 +52,54 @@ void stats(unsigned tab[], unsigned valMax, unsigned nbThrows) {
 
 }
 
-void affichageTab(const unsigned *tab, unsigned taille, unsigned nbLances) {
-    char buffer[13];
-    for (unsigned i = 0; i < taille; ++i) {
-        unsigned tempPourcent = (*(tab + i) * 100) / nbLances;
-        sprintf(buffer, "tableau [%d]", i);
-        printf("%-16s%-8d", buffer, tempPourcent);
-        for (size_t j = 0; j < tempPourcent; ++j) {
-            printf("=");
-        }
-        printf("\n");
-    }
+/**
+ * @brief Calculate de percentage value of a value
+ *
+ * @param value to convert to percent
+ * @param total 100%
+ *
+ * @return the percentage value
+ */
+unsigned calculatePercentage(unsigned value, unsigned total) {
+    return (!total ? total : (value * 100) / total);
 }
 
-unsigned saisieUtilisateur(unsigned min, unsigned max, char *text) {
+/**
+ * @brief display an array
+ *
+ * @param tab       array to display
+ * @param taille    size of the array
+ * @param nbThrows  max number of throws done
+ */
+void displayTab(const unsigned *tab, unsigned taille, unsigned nbThrows) {
+    char buffer[13];
+
+    printf("\n");
+
+    for (unsigned i = 0; i < taille; ++i) {
+        unsigned percentage = calculatePercentage(*(tab + i), nbThrows);
+
+        sprintf(buffer, "tableau [%d]", i);
+        printf("%-16s%-8d", buffer, percentage);
+
+        for (size_t j = 0; j < percentage; ++j) {
+            printf("=");
+        }
+
+        printf("\n");
+    }
+    printf("\n");
+}
+
+/**
+ * @brief Get user input
+ *
+ * @param min   value that can be inputed
+ * @param max   value that can be inputed
+ * @param text  to display
+ * @return the user input
+ */
+unsigned userInput(unsigned min, unsigned max, char *text) {
     bool ok;
     unsigned saisie;
     const char *ERREUR = "/!\\ erreur de saisie\n";
@@ -53,32 +118,32 @@ unsigned saisieUtilisateur(unsigned min, unsigned max, char *text) {
 
 int main() {
 
-    unsigned nbElementsTab = 0,
-             nbLances = 0;
+    unsigned nbElements = 0,
+             nbThrows = 0;
 
-    char refaire;
+    char restart;
     const unsigned MAX_ELEMENT = 100;
     const unsigned MIN_ELEMENT = 0;
-    const unsigned MAX_LANCE = 1000;
-    const unsigned MIN_LANCE = 0;
+    const unsigned MAX_THROW = 1000;
+    const unsigned MIN_THROW = 0;
 
     do {
 
-        nbElementsTab = saisieUtilisateur(MIN_ELEMENT, MAX_ELEMENT, "nbre d'elements ");
+        nbElements = userInput(MIN_ELEMENT, MAX_ELEMENT, "nbre d'elements ");
 
-        unsigned tab[nbElementsTab];
+        unsigned tab[nbElements];
 
-        nbLances = saisieUtilisateur(MIN_LANCE, MAX_LANCE, "nbre de lances");
+        nbThrows = userInput(MIN_THROW, MAX_THROW, "nbre de lances");
 
-        stats(tab, nbElementsTab, nbLances);
+        generateRandom(tab, nbElements, nbThrows);
 
-        affichageTab(tab, nbElementsTab, nbLances);
+        displayTab(tab, nbElements, nbThrows);
 
         printf("voulez-vous recommencer [o:n] :");
-        scanf("%c", &refaire);
+        scanf("%c", &restart);
         VIDER_BUFFER;
 
-    } while (refaire != 'n');
+    } while (restart != 'n');
 
     return 0;
 }
